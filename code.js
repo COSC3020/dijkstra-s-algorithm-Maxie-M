423e0d781f2f4d44540d0260c9b1f1598247e88a
Maxie <144727967+Maxie-M@users.noreply.github.com>
2024-11-08 20:53:33 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,46 @@
-function dijkstra(graph, sourceNode) {
-    return [];
+function dijkstra(graph, sourceNode) 
+{
+    let dist = {};
+    let vis = new Set()
+
+    for (let node in graph)
+    {
+        dist[node] = Infinity;
+    }
+    dist[sourceNode] = 0;
+
+    function getClosestUnvisitedNode()
+    {
+        let minDist = Infinity;
+        let clstNode = null;
+
+        for (let node in dist)
+        {
+            If (!vis.has(node) && dist[node] < minDist)
+            {
+                minDist = dist[node];
+                clstNode = node; 
+            }
+        }
+        return clstNode;
+    }
+
+    let currentNode = getClosestUnvisitedNode();
+
+    while (currentNode !== null)
+    {
+        let currDist = dist[currentNode];
+        vis.add(currentNode);
+
+        for (let neighbor in graph[currentNode])
+        {
+            if (!vis.has(neighbor))
+            {
+                let newDist = currDist + graph[currentNode][neighbor];
+                dist[neighbor] = Math.min(dist[neighbor], newDist);
+            }
+        }
+        currentNode = getClosestUnvisitedNode();
+    }
+    return dist;
 }
