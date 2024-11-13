1b910b5696895242b54ccb43c7537294f3edf8e2
Maxie <144727967+Maxie-M@users.noreply.github.com>
2024-11-12 23:28:42 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,35 +1,29 @@
-// code.test.js 
-// Maxie Machado 
-// Dijkstra's Algorithm 
+// code.test.js
+// Maxie Machado
+// Dijkstra's Algorithm
 
-const assert = require('assert');
+const assert = require("assert");
 
-function testDijkstra() {
-        const graph = {
-            A: { B: 4, C: 1 },
-            B: { A: 4, C: 2, D: 5 },
-            C: { A: 1, B: 2, D: 8, E: 10 },
-            D: { B: 5, C: 8, E: 2 },
-            E: { C: 10, D: 2 }
-        };
-        const expectedDistances = { A: 0, B: 3, C: 1, D: 7, E: 9 };
-        const result = dijkstra(graph, 'A');
-        assert.deepStrictEqual(result, expectedDistances, 'Test 1 failed');
-
-        const graph = {
-            A: {B: 1 },
-            B: { A: 1, C: 2},
-            C: { B: 2 },
-            D: {}
-    };
-        const expectedDistances = { A: 0, B: 1, C: 3, D: Infinity };
-        const result2 = dijkstra(graph2, 'A');
-        assert.deepStrictEqual(result2, expectedDistances, 'Test 2 failed');
+const graph1 = {
+    A: { B: 4, C: 1 },
+    B: { A: 4, C: 2, D: 5 },
+    C: { A: 1, B: 2, D: 8, E: 10 },
+    D: { B: 5, C: 8, E: 2 },
+    E: { C: 10, D: 2 },
 };
+const expectedDistances1 = { A: 0, B: 3, C: 1, D: 7, E: 9 };
+const result1 = dijkstra(graph1, "A");
+assert.deepStrictEqual(result1, expectedDistances1, "Test 1 Failed");
 
-try {
-    testDijkstra();
-    console.log("All tests passed!");
-} catch (error) {
-    console.error("test failed:", error);
-}
+const graph2 = {
+    A: { B: 1 },
+    B: { A: 1, C: 2 },
+    C: { B: 2 },
+    D: {},
+};
+const expectedDistances2 = { A: 0, B: 1, C: 3, D: Infinity };
+const result2 = dijkstra(graph2, "A");
+assert.deepStrictEqual(result2, expectedDistances2, "Test 2 Failed");
+
+console.log("All tests passed!");
+
